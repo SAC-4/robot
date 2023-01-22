1f1ea27c72261bb375ca9e2fa043990a6779ef04
SAC-4 <119978243+SAC-4@users.noreply.github.com>
2023-01-21 20:17:12 -0500

    added intake code
    added intake off/on and pivot angle up/down

--- intake.java
+++ intake.java
@@ -1,19 +1,47 @@
-public class intake {
+public class Intake {
+    boolean intakeon = false;
+    boolean intakeup = false;
 
-    static void intakeon(String I) {
-       if (I == )
-       System.out.print("Intake Running");
-    }
+    public void intake1() {
+         
 
-    static void rollers(String a) {
-        if (a == "down") {
-            System.out.println("rollers up ");
+        if (intakeon) {
+            intakeon = false;
+            System.out.println("off");
+        } else {
+            intakeon = true;
+            System.out.println("on");
+        }
 
-        } else 
-        System.out.println("rollers down");
+}
+public void intakeangle() {
+    
+    
+    if (intakeup) {
+        intakeup = false;
+        System.out.println("angle up");
+}   else {intakeup = true;
+    System.out.println("angle down");   
+}
+}
 
-    }
+}
+
+      // System.out.println("Here");
+      // if (I == "enable") {
+       // System.out.println("Intake Running");
+      // } else
+      // System.out.println("Intake disabled");
+       
+
+    //static void rollers(String a) {
+       // if (a == "toggleup") {
+         //   System.out.println("rollers up ");
+
+       // } else 
+        //System.out.println("rollers down");
+
     //Make a functon to toggle the intake on and off
     //Make a function to toggle the angle of the rollers up/down
     
-}
+
--- robot.java
+++ robot.java
@@ -1,10 +1,16 @@
 public class robot {
     public static void main(String[] args) {
+       
         Drivetrain.function1();
         Drivetrain.moving(-12);
+        
+        Intake intake=new Intake();
+        intake.intake1();
+        intake.intake1();
+        intake.intake1();
 
-        intake.intakeon();
-        intake.rollers("off");
+       // Intake.intakeon("enable");
+       // Intake.rollers("toggleup");
     }
     
 }
